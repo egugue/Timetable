ea38a0915ec647ce50bae5a9f178bf256a14b0c1
egugue <toyamaosamuyu@toyamaosamuyu-no-MacBook-Air.local>
2014-12-26 04:20:28 +0900

    resolve #4

---
+++ app/src/main/java/htoyama/timetable/domain/models/BaseInfo.java
@@ -0,0 +1,13 @@
+package htoyama.timetable.domain.models;
+
+/**
+ * Created by toyamaosamuyu on 2014/12/26.
+ */
+public class BaseInfo {
+
+    public int id;
+    public String station;
+    public String train;
+    public String boundForName;
+
+}
---
+++ app/src/main/java/htoyama/timetable/domain/models/Timetable.java
@@ -0,0 +1,8 @@
+package htoyama.timetable.domain.models;
+
+/**
+ * Created by toyamaosamuyu on 2014/12/26.
+ */
+public class Timetable {
+    private static final String TAG = Timetable.class.getSimpleName();
+}
